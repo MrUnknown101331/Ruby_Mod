383d9993f03872f5cfca34001c13fa8dd74312c2
BuildTools <unconfigured@null.spigotmc.org>
2022-10-28 21:20:38 +0530

    Initial commit

---
+++ src/main/java/net/mrunknown/rubymod/RubyModClient.java
@@ -0,0 +1,10 @@
+package net.mrunknown.rubymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RubyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
