2f861d4a9d80131ccced169517829ced0e8cd240
BuildTools <unconfigured@null.spigotmc.org>
2022-10-28 21:22:31 +0530

    Added important commnet

--- src/main/java/net/mrunknown/rubymod/RubyMod.java
+++ src/main/java/net/mrunknown/rubymod/RubyMod.java
@@ -3,7 +3,7 @@ package net.mrunknown.rubymod;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//Very important comment
 public class RubyMod implements ModInitializer {
 	public static final String MOD_ID = "rubymod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
